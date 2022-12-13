e51831b05bb5d55b352587af9ea1f7ea9f8e52cb
Andrew <it.andr@outlook.com>
2022-12-13 22:24:02 +0300

    team work ver1

---
+++ Seminar/calc_complex.py
@@ -0,0 +1,12 @@
+#Операция с комплексными числами
+
+def calc_complex_main(a, b, move):
+    if move == '*':
+        return a * b
+    if move == '/':
+        return a / b
+    if move == '+':
+        return a + b
+    if move == '-':
+        return a - b
+calc_complex_main()
