dc3a30b4de6b8f943a4ff6b11a5336a232ade507
Andrew <it.andr@outlook.com>
2022-12-23 18:56:00 +0300

    tictac

--- Seminar/telegram_bot/main.py
+++ Seminar/telegram_bot/main.py
@@ -4,6 +4,7 @@ from config import TOKEN_API
 from aiogram.utils.markdown import link
 import markups as nav
 import random
+# from tictactoe import main
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -14,7 +15,7 @@ dp = Dispatcher(bot)
 HELP_COMMAND = """
 /start - начало работы с ботом
 /help - список команд
-/description - о работе бота
+/tictactoe - о работе бота
 """
 
 
@@ -41,10 +42,58 @@ async def send_helper(message: types.Message):
     await message.reply(text=HELP_COMMAND)
 
 
-@dp.message_handler(commands=['description'])
+@dp.message_handler(commands=['tictactoe'])
 async def description(message: types.Message):
-    await message.answer("The bot will improve itself every day in search of something interesting")
-    await message.delete()
+    board = list(range(1, 10))
+    wins_coordinate = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 5, 9), (3, 5, 7), (1, 4, 7), (2, 5, 8), (3, 6, 9)]
+
+    def draw_board():
+        print('-------------')
+        for i in range(3):
+            print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
+        print('-------------')
+
+    def take_input(player_signal):
+        while True:
+            value = input('Выберите позицию: ' + player_signal + ' ? ')
+            if not (value in '123456789'):
+                print('Такого числа нет в таблице. Повторите пожалуйта!')
+                continue  # Возвращает на начало цикла while
+            value = int(value)
+            if str(board[value - 1]) in 'XO':
+                print('Эта клетка уже занята')
+                continue
+            board[value - 1] = player_signal
+            break
+
+    def check_win():
+        for each in wins_coordinate:
+            if (board[each[0] - 1]) == (board[each[1] - 1]) == (board[each[2] - 1]):
+                return board[each[1] - 1]
+        else:
+            return False
+
+    def main():
+        counter = 0
+        while True:
+            draw_board()
+            if counter % 2 == 0:
+                take_input('X')
+            else:
+                take_input('O')
+            if counter > 3:
+                winner = check_win()
+                if winner:
+                    draw_board()
+                    print(winner, 'выиграл!')
+                    break
+            counter += 1
+            if counter > 8:
+                draw_board()
+                print('Ничья!')
+                break
+
+    main()
 
 
 @dp.message_handler()
