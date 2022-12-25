a3e53beb720429cdbf4ed27249e12ee9709f73b7
Andrew <it.andr@outlook.com>
2022-12-25 17:23:53 +0300

    vers2

--- Seminar/telegram_bot/config.py
+++ Seminar/telegram_bot/config.py
@@ -1,2 +1,10 @@
 # Бот - сервер, который будет взаимодействовать с API Telegram
-TOKEN_API = "5609251299:AAGCcvHetGHdJ2HmPSSdYayJUDR6k97NlJ8"
+TOKEN_API = ""
+
+
+# import requests
+# city = input()
+# r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=5ad7b8fca6310aff7d3bdd2d7d6a2631&units=metric")
+# data = r.json()
+#
+# print(data)
